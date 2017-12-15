12b180e7739c4649b2dab2bd49a8ef492b258410
Ola96 <32741661+Ola96@users.noreply.github.com>
2017-12-15 22:50:02 +0100

    Add files via upload

---
+++ zajęcia7_12/src/sample/DataContainer.java
@@ -0,0 +1,29 @@
+package sample;
+
+import java.util.List;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+
+public class DataContainer {
+
+    protected ObservableList<Student> students;
+
+    public ObservableList<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = FXCollections.observableArrayList(students);
+    }
+
+    public DataContainer() {
+        students = FXCollections.observableArrayList();
+    }
+}
+
+///              effektywnosc    zrozumienie    kompleksowosc
+//StackOverFlow      +                -            -
+//JavaDoc            ?                -            +
+//Tutorial           -                +
